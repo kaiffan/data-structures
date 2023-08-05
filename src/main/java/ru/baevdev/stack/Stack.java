d7c31551cbcf7fe550e980509945fb4127c8907f
Kaiffan <pavel.baev44@gmail.com>
2023-08-05 18:20:59 +0300

    Complete stack implementation

---
+++ src/main/java/ru/baevdev/stack/Stack.java
@@ -0,0 +1,54 @@
+package ru.baevdev.stack;
+
+public class Stack<T>{
+
+    private Element head;
+
+    class Element {
+        T data;
+        Element next;
+
+        public Element(T data, Element next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "Element{" +
+                    "data=" + data +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public void push(T data) {
+        head = new Element(data, head);
+    }
+
+    public T pop() {
+        if (head == null) {
+            throw new IllegalArgumentException("Head - null");
+        }
+        T dataHead = head.data;
+        head = head.next;
+        return dataHead;
+    }
+
+    public T peek() {
+        return head.data;
+    }
+
+    public int size() {
+        int counter = 1;
+        Element tmpHead = head;
+
+        while (tmpHead.next != null) {
+            counter++;
+            tmpHead = tmpHead.next;
+        }
+        return counter;
+    }
+
+
+}
